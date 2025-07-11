07a7e4d67ded33227ed50d3813ba7a268bbaea29
iamvalson <itzvalsonwrld@gmail.com>
2025-07-11 23:47:53 +0100

    New Screens Upload

--- mobile/edu-guard-ai/app/(auth)/onboarding.tsx
+++ mobile/edu-guard-ai/app/(auth)/onboarding.tsx
@@ -1,70 +1,75 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Onborading() {
   const router = useRouter();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#fff",
-      }}
-    >
-      <Image
-        source={require("../../assets/images/onboarding-image.png")}
-        style={{ width: "100%", height: 300, backgroundColor: "#102e30" }}
-      />
-
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#F9FAFB" }}>
       <View
         style={{
           flex: 1,
-          alignItems: "center",
-          justifyContent: "space-between",
           backgroundColor: "#fff",
-          paddingVertical: 20,
-          paddingHorizontal: 20,
         }}
       >
-        <View style={{ rowGap: 10, alignItems: "center" }}>
-          <Text style={{ fontSize: 32, fontWeight: 700, textAlign: "center" }}>
-            Welcome to EduGuard AI
-          </Text>
-          <Text
-            style={{
-              fontSize: 18,
-              textAlign: "center",
-              color: "#333",
-              fontWeight: 500,
-            }}
-          >
-            Learn to identify and avoid online scams with our AI-powered
-            education platform
-          </Text>
-        </View>
+        <Image
+          source={require("../../assets/images/onboarding-image.png")}
+          style={{ width: "100%", height: 300, backgroundColor: "#102e30" }}
+        />
 
-        <TouchableOpacity
+        <View
           style={{
-            backgroundColor: "#102e30",
-            width: "100%",
-            borderRadius: 10,
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "space-between",
+            backgroundColor: "#fff",
+            paddingVertical: 20,
+            paddingHorizontal: 20,
           }}
-          onPress={() => router.push("/signup")}
         >
-          <Text
+          <View style={{ rowGap: 10, alignItems: "center" }}>
+            <Text
+              style={{ fontSize: 32, fontWeight: 700, textAlign: "center" }}
+            >
+              Welcome to EduGuard AI
+            </Text>
+            <Text
+              style={{
+                fontSize: 18,
+                textAlign: "center",
+                color: "#333",
+                fontWeight: 500,
+              }}
+            >
+              Learn to identify and avoid online scams with our AI-powered
+              education platform
+            </Text>
+          </View>
+
+          <TouchableOpacity
             style={{
-              textAlign: "center",
-              fontSize: 18,
-              fontWeight: 600,
-              color: "#fff",
-              paddingVertical: 15,
+              backgroundColor: "#102e30",
+              width: "100%",
+              borderRadius: 10,
             }}
+            onPress={() => router.push("/signup")}
           >
-            Get Started
-          </Text>
-        </TouchableOpacity>
+            <Text
+              style={{
+                textAlign: "center",
+                fontSize: 18,
+                fontWeight: 600,
+                color: "#fff",
+                paddingVertical: 15,
+              }}
+            >
+              Get Started
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
--- mobile/edu-guard-ai/app/(auth)/signup.tsx
+++ mobile/edu-guard-ai/app/(auth)/signup.tsx
@@ -12,97 +12,66 @@ import {
 import React, { useState } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Signup() {
   const router = useRouter();
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={{ flex: 1 }}
-      keyboardVerticalOffset={Platform.OS === "ios" ? 10 : 60}
-    >
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <ScrollView
-          contentContainerStyle={{
-            flexGrow: 1,
-            paddingVertical: 20,
-            marginTop: 10,
-            paddingHorizontal: 20,
-            justifyContent: "space-between",
-          }}
-          keyboardShouldPersistTaps="handled"
-        >
-          {/* Header */}
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#F9FAFB" }}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 10 : 60}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <ScrollView
+            contentContainerStyle={{
+              paddingVertical: 20,
+              marginTop: 10,
+              paddingHorizontal: 20,
+              justifyContent: "space-between",
+            }}
+            keyboardShouldPersistTaps="handled"
           >
-            <TouchableOpacity onPress={() => router.back()}>
-              <Ionicons name="chevron-back-outline" size={30} color={"#333"} />
-            </TouchableOpacity>
-            <Text style={{ fontSize: 22, fontWeight: 700 }}>EduGuard AI</Text>
-            <View style={{ width: 24 }}></View>
-          </View>
-
-          {/* Header Text */}
-
-          <View style={{ marginTop: 30, alignItems: "center", rowGap: 20 }}>
-            <Text style={{ fontSize: 24, fontWeight: 700 }}>
-              Create Account
-            </Text>
-            <Text
-              style={{ fontSize: 17, fontWeight: 500, textAlign: "center" }}
+            {/* Header */}
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
             >
-              Sign up to start learning about online safety
-            </Text>
-          </View>
-
-          {/* Content */}
-          <View style={{ flex: 1, justifyContent: "space-between" }}>
-            {/* Form */}
-
-            <View style={{ marginTop: 20 }}>
-              <View style={{ rowGap: 10, marginBottom: 20 }}>
-                <Text style={{ fontSize: 18, fontWeight: 600 }}>Name</Text>
-                <TextInput
-                  placeholder="Enter your name"
-                  placeholderTextColor={"#67788b"}
-                  style={{
-                    backgroundColor: "#dbdfe4",
-                    paddingHorizontal: 20,
-                    paddingVertical: 15,
-                    fontSize: 18,
-                    color: "#67788b",
-                    borderRadius: 10,
-                    fontWeight: 500,
-                  }}
+              <TouchableOpacity onPress={() => router.back()}>
+                <Ionicons
+                  name="chevron-back-outline"
+                  size={30}
+                  color={"#333"}
                 />
-              </View>
+              </TouchableOpacity>
+              <Text style={{ fontSize: 22, fontWeight: 700 }}>EduGuard AI</Text>
+              <View style={{ width: 24 }}></View>
+            </View>
 
-              <View style={{ rowGap: 10, marginBottom: 20 }}>
-                <Text style={{ fontSize: 18, fontWeight: 600 }}>Email</Text>
-                <TextInput
-                  placeholder="Enter your email"
-                  placeholderTextColor={"#67788b"}
-                  style={{
-                    backgroundColor: "#dbdfe4",
-                    paddingHorizontal: 20,
-                    paddingVertical: 15,
-                    fontSize: 18,
-                    color: "#67788b",
-                    borderRadius: 10,
-                    fontWeight: 500,
-                  }}
-                  inputMode="email"
-                />
-              </View>
+            {/* Header Text */}
 
-              <View style={{ rowGap: 10, marginBottom: 20 }}>
-                <Text style={{ fontSize: 18, fontWeight: 600 }}>Password</Text>
-                <View style={{ position: "relative" }}>
+            <View style={{ marginTop: 30, alignItems: "center", rowGap: 20 }}>
+              <Text style={{ fontSize: 24, fontWeight: 700 }}>
+                Create Account
+              </Text>
+              <Text
+                style={{ fontSize: 17, fontWeight: 500, textAlign: "center" }}
+              >
+                Sign up to start learning about online safety
+              </Text>
+            </View>
+
+            {/* Content */}
+            <View style={{ flex: 1, justifyContent: "space-between" }}>
+              {/* Form */}
+
+              <View style={{ marginTop: 20 }}>
+                <View style={{ rowGap: 10, marginBottom: 20 }}>
+                  <Text style={{ fontSize: 18, fontWeight: 600 }}>Name</Text>
                   <TextInput
-                    placeholder="Enter your password"
+                    placeholder="Enter your name"
                     placeholderTextColor={"#67788b"}
                     style={{
                       backgroundColor: "#dbdfe4",
@@ -113,65 +82,105 @@ export default function Signup() {
                       borderRadius: 10,
                       fontWeight: 500,
                     }}
-                    secureTextEntry={!showPassword}
                   />
-                  <TouchableOpacity
+                </View>
+
+                <View style={{ rowGap: 10, marginBottom: 20 }}>
+                  <Text style={{ fontSize: 18, fontWeight: 600 }}>Email</Text>
+                  <TextInput
+                    placeholder="Enter your email"
+                    placeholderTextColor={"#67788b"}
                     style={{
-                      position: "absolute",
-                      right: 15,
-                      top: 15,
+                      backgroundColor: "#dbdfe4",
+                      paddingHorizontal: 20,
+                      paddingVertical: 15,
+                      fontSize: 18,
+                      color: "#67788b",
+                      borderRadius: 10,
+                      fontWeight: 500,
                     }}
-                    onPress={() => setShowPassword(!showPassword)}
-                  >
-                    <Ionicons
-                      name={showPassword ? "eye-off" : "eye"}
-                      size={24}
-                      color="#263238"
+                    inputMode="email"
+                  />
+                </View>
+
+                <View style={{ rowGap: 10, marginBottom: 20 }}>
+                  <Text style={{ fontSize: 18, fontWeight: 600 }}>
+                    Password
+                  </Text>
+                  <View style={{ position: "relative" }}>
+                    <TextInput
+                      placeholder="Enter your password"
+                      placeholderTextColor={"#67788b"}
+                      style={{
+                        backgroundColor: "#dbdfe4",
+                        paddingHorizontal: 20,
+                        paddingVertical: 15,
+                        fontSize: 18,
+                        color: "#67788b",
+                        borderRadius: 10,
+                        fontWeight: 500,
+                      }}
+                      secureTextEntry={!showPassword}
                     />
-                  </TouchableOpacity>
+                    <TouchableOpacity
+                      style={{
+                        position: "absolute",
+                        right: 15,
+                        top: 15,
+                      }}
+                      onPress={() => setShowPassword(!showPassword)}
+                    >
+                      <Ionicons
+                        name={showPassword ? "eye-off" : "eye"}
+                        size={24}
+                        color="#263238"
+                      />
+                    </TouchableOpacity>
+                  </View>
                 </View>
               </View>
-            </View>
 
-            {/* Submit Button */}
-            <View style={{ rowGap: 20, alignItems: "center" }}>
-              <TouchableOpacity
-                style={{ maxWidth: "70%" }}
-                onPress={() => router.push("/login")}
-              >
-                <Text
-                  style={{
-                    textDecorationLine: "underline",
-                    textAlign: "center",
-                    fontSize: 15,
-                  }}
+              {/* Submit Button */}
+              <View style={{ rowGap: 20, alignItems: "center" }}>
+                <TouchableOpacity
+                  style={{ maxWidth: "70%" }}
+                  onPress={() => router.push("/login")}
                 >
-                  Already have an account? Login
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  backgroundColor: "#102e30",
-                  width: "100%",
-                  borderRadius: 10,
-                }}
-              >
-                <Text
+                  <Text
+                    style={{
+                      textDecorationLine: "underline",
+                      textAlign: "center",
+                      fontSize: 15,
+                    }}
+                  >
+                    Already have an account? Login
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
                   style={{
-                    textAlign: "center",
-                    fontSize: 18,
-                    fontWeight: 600,
-                    color: "#fff",
-                    paddingVertical: 15,
+                    backgroundColor: "#102e30",
+                    width: "100%",
+                    borderRadius: 10,
                   }}
+                  onPress={() => router.push("/(tabs)")}
                 >
-                  Register
-                </Text>
-              </TouchableOpacity>
+                  <Text
+                    style={{
+                      textAlign: "center",
+                      fontSize: 18,
+                      fontWeight: 600,
+                      color: "#fff",
+                      paddingVertical: 15,
+                    }}
+                  >
+                    Register
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
-          </View>
-        </ScrollView>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+          </ScrollView>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
